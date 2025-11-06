e6eab42ca183452637a41d186c9c05ea5f6a31ed
abhishekchaudhary2810 <abhisheksinghwall@gmail.com>
2025-11-06 19:21:54 +0000

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,102 @@
+provider "aws" {
+  region = "eu-west-1"
+}
+
+# 1. VPC
+resource "aws_vpc" "main_vpc" {
+  cidr_block = "10.0.0.0/16"
+  tags = {
+    Name = "main-vpc"
+  }
+}
+
+# 2. Subnet
+resource "aws_subnet" "main_subnet" {
+  vpc_id            = aws_vpc.main_vpc.id
+  cidr_block        = "10.0.1.0/24"
+  availability_zone = "eu-west-1a"
+  tags = {
+    Name = "main-subnet"
+  }
+}
+
+# 3. Internet Gateway
+resource "aws_internet_gateway" "main_igw" {
+  vpc_id = aws_vpc.main_vpc.id
+  tags = {
+    Name = "main-igw"
+  }
+}
+
+# 4. Route Table
+resource "aws_route_table" "main_route_table" {
+  vpc_id = aws_vpc.main_vpc.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.main_igw.id
+  }
+  tags = {
+    Name = "main-route-table"
+  }
+}
+
+# 5. Route Table Association
+resource "aws_route_table_association" "main_rta" {
+  subnet_id      = aws_subnet.main_subnet.id
+  route_table_id = aws_route_table.main_route_table.id
+}
+
+# 6. Security Group
+resource "aws_security_group" "web_sg" {
+  name        = "web-sg"
+  description = "Allow SSH and HTTP"
+  vpc_id      = aws_vpc.main_vpc.id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 3000
+    to_port     = 3000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "web-sg"
+  }
+}
+
+# 7. Key Pair
+resource "aws_key_pair" "deployer" {
+  key_name   = "my-ec2-key"
+  public_key = file("~/.ssh/my-ec2-key.pub")
+}
+
+
+# 8. EC2 Instance
+resource "aws_instance" "web-server" {
+  ami           = "ami-0025245f3ca0bcc82" # Amazon Linux 2 AMI
+  instance_type = "t2.micro"
+  subnet_id     = aws_subnet.main_subnet.id
+  key_name      = aws_key_pair.deployer.key_name
+  vpc_security_group_ids = [aws_security_group.web_sg.id]
+  associate_public_ip_address = "true"
+
+
+  tags = {
+    Name = "web-server"
+  }
+}
+
